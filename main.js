f74c55494765e9ce8d17b4728d3e47009b3673db
Freienstein <theFreienstein@gmail.com>
2016-11-27 03:54:44 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ GoldenCookieClick = function () {
   if (Game.shimmers[0] != null) {
     Game.shimmers[0].pop();
     if (Game.hasBuff('Cookie storm')) setTimeout(GoldenCookieClick, 5);
-    else setTimeout(GoldenCookieClick, 5000);
   }
+  else setTimeout(GoldenCookieClick, 5000);
 }
 
 GoldenCookieClick();
