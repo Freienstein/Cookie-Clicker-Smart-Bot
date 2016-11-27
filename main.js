42581904c65d14cbf05a14fa83bb2c771a88d9ac
Freienstein <theFreienstein@gmail.com>
2016-11-27 01:58:13 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,1 @@
-Cache = {};
-
-Cache.AddQueue = function() {
-	Cache.Queue = document.createElement('script');
-	Cache.Queue.type = 'text/javascript';
-	Cache.Queue.setAttribute('src', 'http://aktanusa.github.io/CookieMonster/queue/queue.js');
-	document.head.appendChild(CM.Cache.Queue);
-}
-
-Cache.AddQueue();
-
 Game.ClickCookie();
