16561768256a36d886f0ed329bfc2868192774fa
Freienstein <theFreienstein@gmail.com>
2016-11-27 04:07:07 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 var autoClick = setInterval(Game.ClickCookie, 4);
 
 GoldenCookieClick = function () {
-  if (Game.shimmers[0] != null) {Game.shimmers[0].pop()};
-  setTimeout(GoldenCookieClick, 5000);
 }
